683a3c0afdd83e7c1f69d3bb1d43349cfc96b24a
Kamate2025 <kamatentuyenabo184@gmail.com>
2026-03-21 20:48:14 +0300

    Designed homepage

--- rental_system_main/views.py
+++ rental_system_main/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def homepage(request):
-    return render(request, 'base.html')
+    return render(request, 'homepage.html')
+
+def learn(request):
+    return render(request, 'learn.html')
